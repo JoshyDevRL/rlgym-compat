d6a3f11b962d7f5d98185a00f75dd58d6cb5ead1
Jonathan Keegan <jpk3.14@gmail.com>
2024-08-25 23:01:40 -0400

    v5 changes

--- rlgym_compat/game_state.py
+++ rlgym_compat/game_state.py
@@ -1,67 +1,145 @@
-import numpy as np
 from typing import List, Optional
 
-from rlbot.utils.structures.game_data_struct import GameTickPacket, FieldInfoPacket, PlayerInfo
+import numpy as np
+from rlbot.flat import AirState, FieldInfo, GameTickPacket, PlayerInfo
 
 from .physics_object import PhysicsObject
 from .player_data import PlayerData
 
 
 class GameState:
-    def __init__(self, game_info: FieldInfoPacket):
-        self.game_type: int = 0 # TODO: perhaps update this according to match settings
+    def __init__(self, field_info: FieldInfo, tick_skip=8):
+        self.game_type: int = 0
         self.blue_score = 0
         self.orange_score = 0
         self.last_touch: Optional[int] = -1
-        
+
         self.players: List[PlayerData] = []
-        self._on_ground_ticks = np.zeros(64)
-        self._air_time_since_jump = np.zeros(64)
+        self._total_players = 64
+        self._ticks_since_jump = np.zeros(self._total_players)
+        self._air_time_since_jump_ended = np.zeros(self._total_players)
+        self._used_double_jump_or_flip = [False for _ in range(self._total_players)]
+        self._last_ball_touched_time = np.zeros(self._total_players)
 
         self.ball: PhysicsObject = PhysicsObject()
         self.inverted_ball: PhysicsObject = PhysicsObject()
 
         # List of "booleans" (1 or 0)
-        self.boost_pads: np.ndarray = np.zeros(game_info.num_boosts, dtype=np.float32)
-        self.inverted_boost_pads: np.ndarray = np.zeros_like(self.boost_pads, dtype=np.float32)
+        self.boost_pads: np.ndarray = np.zeros(
+            len(field_info.boost_pads), dtype=np.float32
+        )
+        self.inverted_boost_pads: np.ndarray = np.zeros_like(
+            self.boost_pads, dtype=np.float32
+        )
+        self.last_frame_num = 0
+        self.tick_skip_time = tick_skip / 120
 
-    def decode(self, packet: GameTickPacket, ticks_elapsed=1, tick_skip=8):
+    def decode(self, packet: GameTickPacket):
+        # Increase number of players' persistent data we are tracking, if necessary
+        while self._total_players < len(packet.players):
+            new_ticks_since_jump = np.zeros(2 * self._total_players)
+            new_ticks_since_jump[: self._total_players] = self._ticks_since_jump
+            self._ticks_since_jump = new_ticks_since_jump
+
+            new_air_time_since_jump_ended = np.zeros(2 * self._total_players)
+            new_air_time_since_jump_ended[: self._total_players] = (
+                self._air_time_since_jump_ended
+            )
+            self._air_time_since_jump_ended = new_air_time_since_jump_ended
+
+            new_last_ball_touched_time = np.zeros(2 * self._total_players)
+            new_last_ball_touched_time[: self._total_players] = (
+                self._last_ball_touched_time
+            )
+            self._last_ball_touched_time = new_last_ball_touched_time
+
+            new_used_double_jump_or_flip = [
+                False for _ in range(2 * self._total_players)
+            ]
+            new_used_double_jump_or_flip[: self._total_players] = (
+                self._used_double_jump_or_flip
+            )
+            self._used_double_jump_or_flip = new_used_double_jump_or_flip
+
+            self._total_players *= 2
+
+        # Get number of ticks elapsed since last decode() call
+        ticks_elapsed = 0
+        if self.last_frame_num > 0:
+            ticks_elapsed = packet.game_info.frame_num - self.last_frame_num
+        self.last_frame_num = packet.game_info.frame_num
+
+        # Set score
         self.blue_score = packet.teams[0].score
         self.orange_score = packet.teams[1].score
 
-        for i in range(packet.num_boost):
-            self.boost_pads[i] = packet.game_boosts[i].is_active
+        # Set boost pads
+        for i, pad in enumerate(packet.boost_pad_states):
+            self.boost_pads[i] = pad.is_active
         self.inverted_boost_pads[:] = self.boost_pads[::-1]
 
-        self.ball.decode_ball_data(packet.game_ball.physics)
+        # Set ball
+        ball = packet.balls[0]
+        self.ball.decode_ball_data(ball.physics)
         self.inverted_ball.invert(self.ball)
 
-        self.players = []
-        latest_touch = packet.game_ball.latest_touch
-        for i in range(packet.num_cars):
-            player = self._decode_player(packet.game_cars[i], i, ticks_elapsed)
-            if latest_touch.time_seconds > 0 and i == latest_touch.player_index and packet.game_info.seconds_elapsed - latest_touch.time_seconds < tick_skip / 120:
-                player.ball_touched = True
-            
-            self.players.append(player)
-        
-        if latest_touch.time_seconds > 0:
+        # Set up touch tracking
+        latest_touch = ball.latest_touch
+        if latest_touch.game_seconds > 0:
             self.last_touch = latest_touch.player_index
-        
+        self._last_ball_touched_time[latest_touch.player_index] = (
+            latest_touch.game_seconds
+        )
 
-    def _decode_player(self, player_info: PlayerInfo, index: int, ticks_elapsed: int) -> PlayerData:
+        # Set players
+        self.players = []
+        for i, player in enumerate(packet.players):
+            player_data = self._decode_player(player, i, ticks_elapsed)
+            # Need to set player data ball touched only if player has touched ball in the last tick_skip ticks
+            if (
+                self._last_ball_touched_time[i] > 0
+                and packet.game_info.seconds_elapsed - self._last_ball_touched_time
+                < self.tick_skip_time
+            ):
+                player_data.ball_touched = True
+
+            self.players.append(player)
+
+    def _decode_player(
+        self, player_info: PlayerInfo, index: int, ticks_elapsed: int
+    ) -> PlayerData:
         player_data = PlayerData()
 
-        player_data.car_data.decode_car_data(player_info.physics)
-        player_data.inverted_car_data.invert(player_data.car_data)
-
-        if player_info.has_wheel_contact:
-            self._on_ground_ticks[index] = 0
-            self._air_time_since_jump[index] = 0
-        else:
-            self._on_ground_ticks[index] += ticks_elapsed
-            if player_info.jumped: # Technically this should only start when you stop holding jump
-                self._air_time_since_jump[index] += ticks_elapsed
+        match player_info.air_state:
+            case AirState.OnGround:
+                if self._ticks_since_jump[index] > 0:
+                    self._ticks_since_jump[index] += ticks_elapsed
+                if self._ticks_since_jump[index] > 6:
+                    # We must really be on ground
+                    self._ticks_since_jump[index] = 0
+                    player_data.on_ground = True
+                self._air_time_since_jump_ended[index] = 0
+            case AirState.Jumping:
+                self._ticks_since_jump[index] += ticks_elapsed
+                self._air_time_since_jump_ended[index] = 0
+                if self._ticks_since_jump[index] > 6:
+                    # We cannot be on the ground (excluding some really weird circumstances)
+                    player_data.on_ground = False
+            case AirState.InAir:
+                player_data.on_ground = False
+                if self._air_time_since_jump_ended[index] > 0:
+                    self._air_time_since_jump_ended += ticks_elapsed
+                self._air_time_since_jump_ended[index] += ticks_elapsed
+                if (
+                    player_info.jumped
+                ):  # Technically this should only start when you stop holding jump
+                    self._air_time_since_jump_ended[index] += ticks_elapsed
+            case AirState.DoubleJumping:
+                self._used_double_jump_or_flip[index] = True
+                player_data.on_ground = False
+            case AirState.Dodging:
+                self._used_double_jump_or_flip[index] = True
+                player_data.on_ground = False
 
         player_data.car_id = index
         player_data.team_num = player_info.team
@@ -69,16 +147,22 @@ class GameState:
         player_data.match_saves = player_info.score_info.saves
         player_data.match_shots = player_info.score_info.shots
         player_data.match_demolishes = player_info.score_info.demolitions
-        if player_data.boost_amount < player_info.boost / 100: # This isn't perfect but with decent fps it'll work
+        if (
+            player_data.boost_amount < player_info.boost / 100
+        ):  # This isn't perfect but with decent fps it'll work
             if player_data.boost_pickups == -1:
                 player_data.boost_pickups = 1
             else:
                 player_data.boost_pickups += 1
-        player_data.is_demoed = player_info.is_demolished
-        player_data.on_ground = player_info.has_wheel_contact or self._on_ground_ticks[index] <= 6
+        player_data.is_demoed = player_info.demolished_timeout > 0
         player_data.ball_touched = False
-        player_data.has_jump = not player_info.jumped
-        player_data.has_flip = self._air_time_since_jump[index] < 150 and not player_info.double_jumped
+        player_data.has_jump = self._ticks_since_jump[index] == 0
+        player_data.has_flip = (
+            self._air_time_since_jump_ended[index] < 150
+            and not self._used_double_jump_or_flip[index]
+        )
         player_data.boost_amount = player_info.boost / 100
+        player_data.car_data.decode_car_data(player_info.physics)
+        player_data.inverted_car_data.invert(player_data.car_data)
 
         return player_data
--- rlgym_compat/physics_object.py
+++ rlgym_compat/physics_object.py
@@ -1,31 +1,47 @@
 import math
+
 import numpy as np
-from rlbot.utils.structures.game_data_struct import Physics, Vector3, Rotator
+from rlbot import flat
 
 
 class PhysicsObject:
-    def __init__(self, position=None, euler_angles=None, linear_velocity=None, angular_velocity=None):
+    def __init__(
+        self,
+        position=None,
+        euler_angles=None,
+        linear_velocity=None,
+        angular_velocity=None,
+    ):
         self.position: np.ndarray = position if position else np.zeros(3)
 
         # ones by default to prevent mathematical errors when converting quat to rot matrix on empty physics state
         self.quaternion: np.ndarray = np.ones(4)
 
-        self.linear_velocity: np.ndarray = linear_velocity if linear_velocity else np.zeros(3)
-        self.angular_velocity: np.ndarray = angular_velocity if angular_velocity else np.zeros(3)
+        self.linear_velocity: np.ndarray = (
+            linear_velocity if linear_velocity else np.zeros(3)
+        )
+        self.angular_velocity: np.ndarray = (
+            angular_velocity if angular_velocity else np.zeros(3)
+        )
         self._euler_angles: np.ndarray = euler_angles if euler_angles else np.zeros(3)
-        self._rotation_mtx: np.ndarray = np.zeros((3,3))
+        self._rotation_mtx: np.ndarray = np.zeros((3, 3))
         self._has_computed_rot_mtx = False
 
         self._invert_vec = np.asarray([-1, -1, 1])
         self._invert_pyr = np.asarray([0, math.pi, 0])
 
-    def decode_car_data(self, car_data: Physics):
+    def decode_car_data(self, car_data: flat.Physics):
         self.position = self._vector_to_numpy(car_data.location)
         self._euler_angles = self._rotator_to_numpy(car_data.rotation)
         self.linear_velocity = self._vector_to_numpy(car_data.velocity)
         self.angular_velocity = self._vector_to_numpy(car_data.angular_velocity)
+        self._rotation_mtx = self.rotation_mtx()
+        self.quaternion = rotation_to_quaternion(self._rotation_mtx)
+        # self._rotation_mtx = self.rotation_mtx()
+        # # self._has_computed_rot_mtx = True
+        # self.quaternion = np.ones(4) * 1000  # try to break it if quat is used since it isn't accurate
 
-    def decode_ball_data(self, ball_data: Physics):
+    def decode_ball_data(self, ball_data: flat.Physics):
         self.position = self._vector_to_numpy(ball_data.location)
         self.linear_velocity = self._vector_to_numpy(ball_data.velocity)
         self.angular_velocity = self._vector_to_numpy(ball_data.angular_velocity)
@@ -35,6 +51,10 @@ class PhysicsObject:
         self._euler_angles = other.euler_angles() + self._invert_pyr
         self.linear_velocity = other.linear_velocity * self._invert_vec
         self.angular_velocity = other.angular_velocity * self._invert_vec
+        self._rotation_mtx = self.rotation_mtx()
+        self.quaternion = rotation_to_quaternion(self._rotation_mtx)
+        # self._rotation_mtx = self.rotation_mtx()
+        # self._has_computed_rot_mtx = True
 
     # pitch, yaw, roll
     def euler_angles(self) -> np.ndarray:
@@ -60,7 +80,9 @@ class PhysicsObject:
         return self.rotation_mtx()[:, 0]
 
     def right(self) -> np.ndarray:
-        return self.rotation_mtx()[:, 1] * -1   # These are inverted compared to rlgym because rlbot reasons
+        return (
+            self.rotation_mtx()[:, 1] * -1
+        )  # These are inverted compared to rlgym because rlbot reasons
 
     def left(self) -> np.ndarray:
         return self.rotation_mtx()[:, 1]
@@ -68,10 +90,10 @@ class PhysicsObject:
     def up(self) -> np.ndarray:
         return self.rotation_mtx()[:, 2]
 
-    def _vector_to_numpy(self, vector: Vector3):
+    def _vector_to_numpy(self, vector: flat.Vector3):
         return np.asarray([vector.x, vector.y, vector.z])
 
-    def _rotator_to_numpy(self, rotator: Rotator):
+    def _rotator_to_numpy(self, rotator: flat.Rotator):
         return np.asarray([rotator.pitch, rotator.yaw, rotator.roll])
 
     def _euler_to_rotation(self, pyr: np.ndarray):
@@ -100,3 +122,39 @@ class PhysicsObject:
         theta[2, 2] = CP * CR
 
         return theta
+
+
+def rotation_to_quaternion(m: np.ndarray) -> np.ndarray:
+    trace = np.trace(m)
+    q = np.zeros(4)
+
+    if trace > 0:
+        s = (trace + 1) ** 0.5
+        q[0] = s * 0.5
+        s = 0.5 / s
+        q[1] = (m[2, 1] - m[1, 2]) * s
+        q[2] = (m[0, 2] - m[2, 0]) * s
+        q[3] = (m[1, 0] - m[0, 1]) * s
+    else:
+        if m[0, 0] >= m[1, 1] and m[0, 0] >= m[2, 2]:
+            s = (1 + m[0, 0] - m[1, 1] - m[2, 2]) ** 0.5
+            inv_s = 0.5 / s
+            q[1] = 0.5 * s
+            q[2] = (m[1, 0] + m[0, 1]) * inv_s
+            q[3] = (m[2, 0] + m[0, 2]) * inv_s
+            q[0] = (m[2, 1] - m[1, 2]) * inv_s
+        elif m[1, 1] > m[2, 2]:
+            s = (1 + m[1, 1] - m[0, 0] - m[2, 2]) ** 0.5
+            inv_s = 0.5 / s
+            q[1] = (m[0, 1] + m[1, 0]) * inv_s
+            q[2] = 0.5 * s
+            q[3] = (m[1, 2] + m[2, 1]) * inv_s
+            q[0] = (m[0, 2] - m[2, 0]) * inv_s
+        else:
+            s = (1 + m[2, 2] - m[0, 0] - m[1, 1]) ** 0.5
+            inv_s = 0.5 / s
+            q[1] = (m[0, 2] + m[2, 0]) * inv_s
+            q[2] = (m[1, 2] + m[2, 1]) * inv_s
+            q[3] = 0.5 * s
+            q[0] = (m[1, 0] - m[0, 1]) * inv_s
+    return -q
