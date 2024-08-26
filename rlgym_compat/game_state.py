a2f3b04300d40e43e6204f5f4e28283c8e37784f
Jonathan Keegan <jpk3.14@gmail.com>
2024-08-26 14:22:30 -0400

    remember to index into arrays

--- rlgym_compat/game_state.py
+++ rlgym_compat/game_state.py
@@ -114,7 +114,10 @@ class GameState:
             case AirState.OnGround:
                 if self._ticks_since_jump[index] > 0:
                     self._ticks_since_jump[index] += ticks_elapsed
-                if self._ticks_since_jump[index] > 6 or self._ticks_since_jump == 0:
+                if (
+                    self._ticks_since_jump[index] > 6
+                    or self._ticks_since_jump[index] == 0
+                ):
                     # We must really be on ground
                     self._ticks_since_jump[index] = 0
                     player_data.on_ground = True
